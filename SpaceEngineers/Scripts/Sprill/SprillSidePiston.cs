aea98c6b26996234d0b96569955a4780cb6c7315
Sebastian Weis <sebastian.weis@gmx.at>
2022-03-29 20:58:47 +0200

    Improve Sprill Sidepiston script

--- SpaceEngineers/Scripts/Sprill/SprillSidePiston.cs
+++ SpaceEngineers/Scripts/Sprill/SprillSidePiston.cs
@@ -13,65 +13,135 @@ namespace SpaceEngineers.Scripts.Sprill {
 
         public bool firstOut = true;
 
+        public float lastAngle = -1.0f;
+
+        public Stage stage = Stage.StopRotation;
+
+        private bool extend = true;
+
         public void Main() {
             try {
                 firstOut = true;
-                IMyPistonBase mainPiston = GridTerminalSystem.GetBlockWithName("Sprill Main Piston") as IMyPistonBase;
-                List<IMyPistonBase> sidePistons = GetSidePistons().ToList();
-                MyEcho($"Main Piston Status: {mainPiston.Status.ToString()}");
-                MyEcho($"Main Piston DetailInfo: {mainPiston.DetailedInfo}");
+                MyEcho($"Current stage: {stage}");
+                MyEcho($"Is Extending: {extend}");
                 MyEcho("");
-                PrintSidePistoninfos(sidePistons);
-                if (mainPiston.Status == PistonStatus.Extended) {
-                    ExtendSidePistons(sidePistons);
-                    mainPiston.Velocity = -0.025f;
-                } else if (mainPiston.Status == PistonStatus.Retracted) {
-                    ExtendSidePistons(sidePistons);
-                    mainPiston.Velocity = 0.025f;
+
+                IMyPistonBase mainPiston = GridTerminalSystem.GetBlockWithName("Sprill Main Piston") as IMyPistonBase;
+                MyEcho($"Main Piston MaxLimit: {mainPiston.MaxLimit.ToString()}");
+                MyEcho($"Main Piston Position: {mainPiston.CurrentPosition}");
+
+                IMyPistonBase mainPiston2 = GridTerminalSystem.GetBlockWithName("Sprill Main Piston 2") as IMyPistonBase;
+                MyEcho($"Main Piston 2 MaxLimit: {mainPiston2.MaxLimit.ToString()}");
+                MyEcho($"Main Piston 2 Position: {mainPiston2.CurrentPosition}");
+                MyEcho("");
+
+                IMyMotorAdvancedStator rotor = GridTerminalSystem.GetBlockWithName("Sprill Rotor") as IMyMotorAdvancedStator;
+                MyEcho($"Rotor Angle: {rotor.Angle.ToString("0.0")}");
+                MyEcho($"Last Iteration Rotor Angle: {lastAngle.ToString("0.000")}");
+                MyEcho($"Rotor Velocity: {rotor.TargetVelocityRPM.ToString("0.000")}");
+                MyEcho("");
+
+                IMyPistonBase sidePiston1 = GridTerminalSystem.GetBlockWithName("Sprill Side Piston 1") as IMyPistonBase;
+                MyEcho($"Side Piston 1 MaxLimit: {sidePiston1.MaxLimit.ToString()}");
+                MyEcho($"Side Piston 1 Position: {sidePiston1.CurrentPosition}");
+
+                IMyPistonBase sidePiston2 = GridTerminalSystem.GetBlockWithName("Sprill Side Piston 2") as IMyPistonBase;
+                MyEcho($"Side Piston 2 MaxLimit: {sidePiston2.MaxLimit.ToString()}");
+                MyEcho($"Side Piston 2 Position: {sidePiston2.CurrentPosition}");
+
+                if (lastAngle < 0.0f) {
+                    lastAngle = rotor.Angle;
                 }
+
+                switch (stage) {
+                    case Stage.StopRotation:
+                        rotor.TargetVelocityRPM = 0.0f;
+                        stage = Stage.StartExtendHorizontal;
+                        break;
+                    case Stage.StartExtendHorizontal:
+                        sidePiston1.Velocity = 0.4f;
+                        sidePiston2.Velocity = 0.4f;
+
+                        sidePiston1.MaxLimit += 1.0f;
+                        sidePiston2.MaxLimit += 1.0f;
+
+                        stage = Stage.FinishExtendHorizontal;
+                        break;
+                    case Stage.FinishExtendHorizontal:
+                        if ((sidePiston1.MaxLimit - sidePiston1.CurrentPosition) > 0.0001f || (sidePiston2.MaxLimit - sidePiston2.CurrentPosition) > 0.0001f) {
+                            lastAngle = rotor.Angle;
+                            return;
+                        }
+                        stage = Stage.Rotate;
+                        break;
+                    case Stage.Rotate:
+                        rotor.TargetVelocityRPM = 0.2f;
+
+                        float diff = rotor.Angle - lastAngle;
+                        MyEcho($"Rotot Angle Change: {diff.ToString()}");
+                        if (diff < -2.0f) {
+                            stage = Stage.ExtendVertical;
+                        }
+                        break;
+                    case Stage.ExtendVertical:
+                        if (extend) {
+                            mainPiston.Velocity = 0.4f;
+                            mainPiston2.Velocity = 0.4f;
+                            if ((35.0f - mainPiston.MaxLimit) > 0.00001f) {
+                                mainPiston.MaxLimit += 1.0f;
+                            } else {
+                                if ((35.0f - mainPiston2.MaxLimit) > 0.00001f) {
+                                    mainPiston2.MaxLimit += 1.0f;
+                                } else {
+                                    extend = false;
+                                    stage = Stage.StopRotation;
+                                    lastAngle = rotor.Angle;
+                                    return;
+                                }
+                            }
+                        } else {
+                            mainPiston.Velocity = -0.4f;
+                            mainPiston2.Velocity = -0.4f;
+                            if (mainPiston2.MaxLimit > 0.00001f) {
+                                mainPiston2.MaxLimit -= 1.0f;
+                            } else {
+                                if ((mainPiston2.MaxLimit - 23.0f) > 0.00001f) {
+                                    mainPiston.MaxLimit -= 1.0f;
+                                } else {
+                                    extend = true;
+                                    stage = Stage.StopRotation;
+                                    lastAngle = rotor.Angle;
+                                    return;
+                                }
+                            }
+                        }
+                        stage = Stage.Rotate;
+                        break;
+                }
+                lastAngle = rotor.Angle;
             } catch (Exception e) {
                 Echo($"Error occured in script: {e.Message}");
             }
         }
 
+        public enum Stage {
+            StopRotation,
+            StartExtendHorizontal,
+            FinishExtendHorizontal,
+            Rotate,
+            ExtendVertical
+        }
+
         private void MyEcho(string msg) {
             Echo(msg);
-            if (Me.SurfaceCount > 0) {
+            if (((IMyTextSurfaceProvider)Me).SurfaceCount > 0) {
                 string extra = "";
-                if (!firstOut && msg.Length != 0) extra = Environment.NewLine;
-                IMyTextSurface surface = Me.GetSurface(0);
+                if (!firstOut) extra = Environment.NewLine;
+                IMyTextSurface surface = ((IMyTextSurfaceProvider)Me).GetSurface(0);
                 surface.ContentType = ContentType.TEXT_AND_IMAGE;
                 surface.WriteText(extra + msg, !firstOut);
                 firstOut = false;
             }
-        }
-
-        private void ExtendSidePistons(List<IMyPistonBase> sidePistons) {
-            for (int i = 0; i < sidePistons.Count; i++) {
-                ExtendSidePiston(sidePistons[i]);
-            }
-        }
-
-        private void PrintSidePistoninfos(List<IMyPistonBase> sidePistons) {
-            for (int i = 0; i < sidePistons.Count; i++) {
-                PrintSidePistonInfo(sidePistons[i]);
-            }
-        }
-
-        private IEnumerable<IMyPistonBase> GetSidePistons() {
-            string sidePiston1Name = "Sprill Side Piston 1";
-            string sidePiston2Name = "Sprill Side Piston 2";
-            yield return GridTerminalSystem.GetBlockWithName(sidePiston1Name) as IMyPistonBase;
-            yield return GridTerminalSystem.GetBlockWithName(sidePiston2Name) as IMyPistonBase;
-        }
-
-        private void ExtendSidePiston(IMyPistonBase sidePiston) {
-            sidePiston.Velocity = 0.25f;
-            sidePiston.MaxLimit += 2.0f;
-        }
-
-        private void PrintSidePistonInfo(IMyPistonBase sidePiston) {
-            MyEcho($"{sidePiston.CustomName} MaxLimit: {sidePiston.MaxLimit.ToString()}");
         }
     }
 }
